df7dcbea1a2d5607b620559f2cdeef9310d7088b
LAPTOP-END1ORH3\seank <seankeiffer@gmail.com>
2018-03-08 01:06:45 +0800

    + Player movement implemented + Player can now crouch + Added dialogue test scene + Added Trigger Detectors + Added comment about TextMeshPro + Added study references for TextMeshPro Camera script + Added reference for dialogue scripts

--- SupplyRunner_Capstone01/Assets/Scripts/Trigger Scripts/OnTriggerDetector.cs
+++ SupplyRunner_Capstone01/Assets/Scripts/Trigger Scripts/OnTriggerDetector.cs
@@ -2,15 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OnTriggerDetector : MonoBehaviour {
+public enum TriggerAction
+{
+	Enter,
+	Stay,
+	Exit
+}
 
-	// Use this for initialization
-	void Start () {
-		
+public class OnTriggerDetector : MonoBehaviour
+{
+	public List<string> Tags;
+
+	public delegate void OnObjectTriggerEnter(GameObject objectEntered);
+	public delegate void OnObjectTriggerStay(GameObject objectEntered);
+	public delegate void OnObjectTriggerExit(GameObject objectEntered);
+	public OnObjectTriggerEnter onObjectTriggerEnter;
+	public OnObjectTriggerStay onObjectTriggerStay;
+	public OnObjectTriggerExit onObjectTriggerExit;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		foreach (string tag in Tags)
+		{
+			if (other.CompareTag(tag))
+			{
+				if (onObjectTriggerEnter != null)
+					onObjectTriggerEnter.Invoke(other.gameObject);
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void OnTriggerStay(Collider other)
+	{
+		foreach (string tag in Tags)
+		{
+			if (other.CompareTag(tag))
+			{
+				if (onObjectTriggerStay != null)
+					onObjectTriggerStay.Invoke(other.gameObject);
+			}
+		}
 	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		foreach (string tag in Tags)
+		{
+			if (other.CompareTag(tag))
+			{
+				if (onObjectTriggerExit != null)
+					onObjectTriggerExit.Invoke(other.gameObject);
+			}
+		}
+	}
+
+
 }
